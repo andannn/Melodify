006adf019af26b2a50b0852abb27eb1d5f7d911b
andannn <jqn296763005@gmail.com>
2024-10-18 09:55:02 +0900

    add dummy data

--- core/data/src/nativeMain/kotlin/com/andannn/melodify/core/data/repository/MediaContentRepositoryImpl.native.kt.kt
+++ core/data/src/nativeMain/kotlin/com/andannn/melodify/core/data/repository/MediaContentRepositoryImpl.native.kt.kt
@@ -1,17 +1,65 @@
 package com.andannn.melodify.core.data.repository
 
 import com.andannn.melodify.core.data.MediaContentRepository
+import com.andannn.melodify.core.data.dummy.mediaItems
 import com.andannn.melodify.core.data.model.AlbumItemModel
 import com.andannn.melodify.core.data.model.ArtistItemModel
 import com.andannn.melodify.core.data.model.AudioItemModel
 import com.andannn.melodify.core.data.model.GenreItemModel
+import io.github.aakira.napier.Napier
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
 
-internal class MediaContentRepositoryImpl(
-) : MediaContentRepository {
+@Serializable
+private data class MediaList(
+    val media: List<MediaItem>
+)
+
+@Serializable
+private data class MediaItem(
+    val id: String,
+    val title: String,
+    val album: String,
+    val artist: String,
+    val genre: String,
+    val source: String,
+    val image: String,
+    val trackNumber: Int,
+    val totalTrackCount: Long,
+    val duration: Long,
+    val site: Long,
+)
+
+private fun MediaItem.toModel(): AudioItemModel {
+    return AudioItemModel(
+        id = id.toLong(),
+        name = title,
+        artWorkUri = source,
+        modifiedDate = -1,
+        album = album,
+        albumId = album.toLong(),
+        artist = artist,
+        artistId = artist.toLong(),
+        cdTrackNumber = trackNumber,
+        discNumberIndex = 0,
+    )
+}
+
+internal class MediaContentRepositoryImpl : MediaContentRepository {
+    private val mediaList = Json.decodeFromString<MediaList>(mediaItems)
+
+    init {
+        Napier.d { "mediaList: $mediaList" }
+    }
+
     override fun getAllMediaItemsFlow(): Flow<List<AudioItemModel>> {
-        return flow { emit(emptyList()) }
+        return flow {
+            emit(
+                mediaList.media.map { it.toModel() }
+            )
+        }
     }
 
     override fun getAllAlbumsFlow(): Flow<List<AlbumItemModel>> {
