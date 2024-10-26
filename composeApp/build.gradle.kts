8b599ba7e1f8cd82adb44cb5822670fa80a1b48c
andannn <jqn296763005@gmail.com>
2024-10-27 01:01:04 +0900

    update version

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -52,8 +52,8 @@ android {
 
     defaultConfig {
         applicationId = "com.andannn.melodify"
-        versionCode = 16
-        versionName = "0.2.0"
+        versionCode = 17
+        versionName = "0.2.1"
 
         signingConfig = signingConfigs.getByName("debug")
     }
