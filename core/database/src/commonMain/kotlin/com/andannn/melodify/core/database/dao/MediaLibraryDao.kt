de97bc8e073e699ed0f71f89a94540f8d9ec6325
andannn <jqn296763005@gmail.com>
2024-11-22 14:51:08 +0900

    clear database before insert

--- core/database/src/commonMain/kotlin/com/andannn/melodify/core/database/dao/MediaLibraryDao.kt
+++ core/database/src/commonMain/kotlin/com/andannn/melodify/core/database/dao/MediaLibraryDao.kt
@@ -1,9 +1,12 @@
 package com.andannn.melodify.core.database.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
+import androidx.room.Query
 import androidx.room.Transaction
+import com.andannn.melodify.core.database.Tables
 import com.andannn.melodify.core.database.entity.AlbumEntity
 import com.andannn.melodify.core.database.entity.ArtistEntity
 import com.andannn.melodify.core.database.entity.GenreEntity
@@ -23,13 +26,30 @@ interface MediaLibraryDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMedias(audios: List<MediaEntity>)
 
+    @Query("DELETE FROM ${Tables.LIBRARY_ALBUM}")
+    suspend fun deleteAllAlbums()
+
+    @Query("DELETE FROM ${Tables.LIBRARY_ARTIST}")
+    suspend fun deleteAllArtists()
+
+    @Query("DELETE FROM ${Tables.LIBRARY_GENRE}")
+    suspend fun deleteAllGenres()
+
+    @Query("DELETE FROM ${Tables.LIBRARY_MEDIA}")
+    suspend fun deleteAllMedias()
+
     @Transaction
-    suspend fun insertLibrary(
+    suspend fun clearAndInsertLibrary(
         albums: List<AlbumEntity>,
         artists: List<ArtistEntity>,
         genres: List<GenreEntity>,
         audios: List<MediaEntity>
     ) {
+        deleteAllAlbums()
+        deleteAllArtists()
+        deleteAllGenres()
+        deleteAllMedias()
+
         insertAlbums(albums)
         insertArtists(artists)
         insertGenres(genres)
--- core/syncer/src/commonMain/kotlin/com/andannn/melodify/core/syncer/MediaLibrarySyncer.kt
+++ core/syncer/src/commonMain/kotlin/com/andannn/melodify/core/syncer/MediaLibrarySyncer.kt
@@ -39,7 +39,7 @@ internal class MediaLibrarySyncerImpl(
         try {
             val mediaData = mediaLibrary.getMediaData()
 
-            mediaLibraryDao.insertLibrary(
+            mediaLibraryDao.clearAndInsertLibrary(
                 mediaData.albumData.toAlbumEntity(),
                 mediaData.artistData.toArtistEntity(),
                 mediaData.genreData.toGenreEntity(),
