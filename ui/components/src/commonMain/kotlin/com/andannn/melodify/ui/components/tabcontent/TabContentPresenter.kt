e748c63579a1f5c36e0d11f2c0048afaaa98c774
andannn <jqn296763005@gmail.com>
2025-03-17 15:22:21 +0900

    fix screenshot test

--- ui/components/src/commonMain/kotlin/com/andannn/melodify/ui/components/tabcontent/TabContentPresenter.kt
+++ ui/components/src/commonMain/kotlin/com/andannn/melodify/ui/components/tabcontent/TabContentPresenter.kt
@@ -3,7 +3,6 @@ package com.andannn.melodify.ui.components.tabcontent
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
