0b10217e0f95206e401d3f0ee7eebddbe9031bac
andannn <jqn296763005@gmail.com>
2025-11-28 17:01:04 +0900

    test: Add test for RetainedModel

--- shared/ui/src/commonMain/kotlin/com/andannn/melodify/ui/core/RetainedModel.kt
+++ shared/ui/src/commonMain/kotlin/com/andannn/melodify/ui/core/RetainedModel.kt
@@ -1,3 +1,7 @@
+/*
+ * Copyright 2025, the Melodify project contributors
+ * SPDX-License-Identifier: Apache-2.0
+ */
 package com.andannn.melodify.ui.core
 
 import androidx.compose.runtime.Composable
@@ -16,7 +20,7 @@ abstract class RetainedModel {
         onClear()
     }
 
-    fun onClear() {}
+    open fun onClear() {}
 }
 
 @Composable
