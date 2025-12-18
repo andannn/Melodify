a0aab1ddf5aa394db4b3ff1c15f6efc4ce4e184a
andannn <jqn296763005@gmail.com>
2025-12-18 15:18:03 +0900

    Use Surface for composable images

--- shared/compose/common/src/commonMain/kotlin/com/andannn/melodify/shared/compose/common/widgets/CircleBoderImage.kt
+++ shared/compose/common/src/commonMain/kotlin/com/andannn/melodify/shared/compose/common/widgets/CircleBoderImage.kt
@@ -4,14 +4,14 @@
  */
 package com.andannn.melodify.shared.compose.common.widgets
 
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import coil3.compose.AsyncImage
 import coil3.compose.LocalPlatformContext
 import coil3.request.ImageRequest
@@ -24,37 +24,34 @@ import org.jetbrains.compose.resources.painterResource
 fun CircleBorderImage(
     model: String?,
     modifier: Modifier = Modifier,
-    shape: Shape = MaterialTheme.shapes.extraSmall,
+    shape: Shape = RoundedCornerShape(8.dp),
     contentScale: ContentScale = ContentScale.Fit,
     defaultImagePlaceholderRes: DrawableResource = Res.drawable.default_image_icon,
 ) {
     if (model == null) return
 
-    Box(
-        modifier =
-            modifier
-                .clip(shape = shape),
+    Surface(
+        modifier = modifier,
+        shape = shape,
     ) {
         if (isIOSCustomMPLibraryUri(model)) {
             IOSMediaArtworkView(
                 modifier = Modifier.fillMaxSize(),
                 coverUri = model,
             )
-
-            return
+        } else {
+            AsyncImage(
+                model =
+                    ImageRequest
+                        .Builder(LocalPlatformContext.current)
+                        .data(model)
+                        .size(Int.MAX_VALUE)
+                        .build(),
+                contentScale = contentScale,
+                placeholder = painterResource(defaultImagePlaceholderRes),
+                error = painterResource(defaultImagePlaceholderRes),
+                contentDescription = "",
+            )
         }
-
-        AsyncImage(
-            model =
-                ImageRequest
-                    .Builder(LocalPlatformContext.current)
-                    .data(model)
-                    .size(Int.MAX_VALUE)
-                    .build(),
-            contentScale = contentScale,
-            placeholder = painterResource(defaultImagePlaceholderRes),
-            error = painterResource(defaultImagePlaceholderRes),
-            contentDescription = "",
-        )
     }
 }
